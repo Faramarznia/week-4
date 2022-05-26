f6342350bbf46d994dde152e8073cdc20725aa2d
Faramarznia <106330674+Faramarznia@users.noreply.github.com>
2022-05-26 19:25:27 +0430

    Create javascript1.js

---
+++ javascript1.js
@@ -0,0 +1,69 @@
+// let faramarzWight = 75;
+// let faramarzHeight = 1.83;
+// const faramarzBmi = faramarzWight / (faramarzHeight * faramarzHeight);
+// console.log(faramarzBmi);
+
+// let ehsanWight = 50;
+// let ehsanHeight = 1.76;
+// const ehsanBmi = ehsanWight / (ehsanHeight * ehsanHeight);
+// console.log(ehsanBmi);
+
+const month = "10";
+
+switch (month) {
+  case "January":
+  case "1":
+    console.log("Spring");
+
+    break;
+  case "February":
+  case "2":
+    console.log("Spring");
+    break;
+  case "March":
+  case "3":
+    console.log("Spring");
+    break;
+  case "April":
+
+  case "4":
+    console.log("Summer");
+    break;
+  case "May":
+  case "5":
+    console.log("Summer");
+    break;
+  case "June":
+  case "6":
+    console.log("Summer");
+    break;
+  case "July":
+  case "7":
+    console.log("Fall");
+    break;
+  case "August":
+  case "8":
+    console.log("Fall");
+    break;
+  case "September":
+  case "9":
+    console.log("Fall");
+    break;
+  case "October":
+  case "10":
+    console.log("Winter");
+    break;
+  case "November":
+  case "11":
+    console.log("Winter");
+    break;
+  case "December":
+  case "12":
+    console.log("Winter");
+    break;
+  default:
+    console.log(
+      "ببین عزیزدل یا اسم ماه رو بنویس یا یه عدد بین یک تا دوازده بده و خل بازی در نیار . مرسی...! اه "
+    );
+}
+
